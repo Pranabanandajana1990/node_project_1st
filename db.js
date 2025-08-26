cf3442fbdd46a8230ea1f9e23c048678d914339d
Pranabananda <sonujana33@gmail.com>
2025-08-26 23:39:47 +0530

    db url update

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 // define the mongodb connection URL
 // const mongoURL = 'mongodb://localhost:27017/mydb1' // we can mydatabasepranab with any other database name
 // const mongoURL = 'mongodb://127.0.0.1:27017/mydb1'; // Use IPv4 instead of 'localhost'
-// const mongoURL = process.env.LOCAL_DB_URL;
-const mongoURL = process.env.DB_URL;
+const mongoURL = process.env.LOCAL_DB_URL;
+//const mongoURL = process.env.DB_URL;
 
 // setup MongoDB connection
 mongoose.connect(mongoURL)
